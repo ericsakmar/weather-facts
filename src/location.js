bc3d648f83fb3433e3aba9b9d006283e71d95b8b
Eric Sakmar <eric.sakmar@gmail.com>
2025-11-28 12:38:30 -0500

    location

---
+++ src/location.js
@@ -0,0 +1,19 @@
+export const getLocation = () => {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      return reject(new Error("Geolocation is not supported by this browser."));
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        reject(new Error("Unable to retrieve location: " + error.message));
+      },
+    );
+  });
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import { getLocation } from "./location";
 import "./style.css";
 import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+getLocation()
+  .then((location) => {
+    console.log("User location:", location);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
